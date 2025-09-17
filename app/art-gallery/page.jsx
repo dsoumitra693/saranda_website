d902f7091ff0ebb1e8d5afb6289aa9d70cc1a60f
Vigneshrdy <vigneshreddy693@gmail.com>
2025-09-17 20:03:51 +0530

    navbar & art

--- app/art-gallery/page.jsx
+++ app/art-gallery/page.jsx
@@ -1,6 +1,33 @@
-export default function Home() {
-  return <>
-    <h1>Saranda Website</h1>
-    <p>Comming Soon</p>
-  </>;
+// export default function Home() {
+//   return <>
+//     <h1>Saranda Website</h1>
+//     <p>Comming Soon</p>
+//   </>;
+// }
+
+"use client";
+
+export default function HeroSection() {
+    return (
+        <div
+            className="min-h-screen w-screen relative overflow-hidden"
+            style={{
+                backgroundImage: 'url("/images/lush-forest-back.png")',
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            }}
+        >
+            
+            <div
+                className="pointer-events-none absolute inset-0 z-10"
+                style={{
+                    backgroundImage: 'url("/images/lush-forest-front.png")',
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                }}
+            />
+        </div>
+    );
 }
