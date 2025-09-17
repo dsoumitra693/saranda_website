15f85df843e33912550b0acda6758e05295ca225
soumo <dsoumitra693@gmail.com>
2025-09-17 18:48:49 +0530

    feat: added UHCSection

--- app/know-us/page.jsx
+++ app/know-us/page.jsx
@@ -1,6 +1,9 @@
-export default function Home() {
-  return <>
-    <h1>Saranda Website</h1>
-    <p>Comming Soon</p>
-  </>;
+"use client";
+
+import UHCSection from "@/components/know-us/UHC-section";
+
+export default function KnowUs() {
+  return <div className="w-full h-screen md:h-fit md:max-w-6xl flex flex-col justify-center items-center p-4 md:mt-10 mx-auto mb-6">
+    <UHCSection />
+  </div>;
 }
