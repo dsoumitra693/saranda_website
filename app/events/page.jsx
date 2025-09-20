b319875ec9e02528f9b83cd5713ee5728518078c
officeneerajsaini <officeneerajsaini@gmail.com>
2025-09-20 23:38:04 +0530

    feat: events page

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,6 +1,33 @@
-export default function Home() {
-  return <>
-    <h1>Saranda Website</h1>
-    <p>Comming Soon</p>
-  </>;
+import { useEventsData } from "@/hooks/useEventsData";
+import EventCard from "@/components/events/eventCard";
+
+export default function Events() {
+  const { events } = useEventsData();
+  return (
+    <>
+      <div
+        className="w-full min-h-screen bg-primary flex flex-col pt-10 bg-[url(/images/mist-forest1.png)] pt-[60px] md:pt-[80px] pb-[30px]"
+        style={{
+          backgroundImage: 'url("/images/mist-forest1.png")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <div className="grid place-items-center">
+          {events.map((event, idx) => (
+            <EventCard
+              key={event.id}
+              title={event.title}
+              description={event.description}
+              imageSrc={event.imageSrc}
+              registerLink={event.registerLink}
+              rulebookLink={event.rulebookLink}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
