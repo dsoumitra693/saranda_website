8a19e7aeecc79df602d47f09e19837e8ecb056da
soumo <dsoumitra693@gmail.com>
2025-09-23 23:45:12 +0530

    feat: adding sanity.io configuration

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,11 @@
+'use client';
 import { Roboto } from "next/font/google";
 import localFont from "next/font/local";
 import Head from "next/head";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import { usePathname } from 'next/navigation';
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -16,21 +18,13 @@ const redwood = localFont({
   variable: "--font-redwood",
 })
 
-export const metadata = {
-  title: {
-    template: "%s | Saranda IITM BS",
-    default: "Saranda House | IITM BS",
-  },
-  description: "Saranda, a house at IIT Madras BS program, fosters creativity and innovation through meetups, workshops, tech showcases, eSports, and cultural events.",
-  authors: [{ name: "Sovit", url: "https://www.linkedin.com/in/5ovit/", }, { name: "Soumitra Das", url: "https://www.linkedin.com/in/soumitra-das-086640314/" }],
-  creator: "Saranda WebOps Team",
-  keywords: [
-    'Saranda', 'House', 'IIT Madras', 'BS Program', 'Community', 'Meetups',
-    'Events', 'Workshops', 'eSports', 'Cultural', 'Tech', 'Students'
-  ],
-};
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  const excludedRoutes = ['/admin', '/coming-soon', '/not-found'];
+  const shouldExcludeLayout = excludedRoutes.some(route => pathname.startsWith(route));
+
   return (
     <html lang="en" className={`${roboto.variable} ${redwood.variable}`}>
       <Head>
@@ -39,9 +33,13 @@ export default function RootLayout({ children }) {
         <link rel="preload" href="/fonts/Realwood Regular.otf" as="font" type="font/otf" crossOrigin="" />
       </Head>
       <body>
-        <Navbar />
-        {children}
-        <Footer />
+        {shouldExcludeLayout ? children : (
+          <>
+            <Navbar />
+            {children}
+            <Footer />
+          </>
+        )}
       </body>
     </html>
   );
