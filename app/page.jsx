75b80316061b681fce0e607a9f64943f6cf76cfa
soumo <dsoumitra693@gmail.com>
2025-09-25 16:07:03 +0530

    remove unwanted files

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 import HeroSection from "@/components/home/hero-section";
-import AboutUs from "@/components/home/about-us";
 
 
 export default function Home() {
   return <>
     <HeroSection />
-    <AboutUs />
   </>;
 }
